b4296a1d96bf00794ee2d4ddfe4440ff6b42f917
gitlyn <lynw345584z@sina.com>
2019-07-31 10:08:11 +0800

    修改数据库配置

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => 'http://106.13.103.253/',
     // 数据库名
     'database'        => '',
     // 用户名
